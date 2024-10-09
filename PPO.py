6f2220c48092b316dea9ab79ea602cf3a39f3810
miraclys <miraclysyunsenl@gmail.com>
2024-10-09 17:26:09 +0800

    feat: finish the PPO

---
+++ PPO.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+from torch.distributions import Categorical
+from torch.distributions import MultivariateNormal
+
+class RolloutBuffer:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.log_probs = []
+        self.rewards = []
+        self.state_values = []
+        self.is_terminals = []
+
+    def clear(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.log_probs[:]
+        del self.rewards[:]
+        del self.state_values[:]
+        del self.is_terminals[:]
+        
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(ActorCritic, self).__init__()
+
+        self.actor = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, action_dim),
+            nn.Softmax(dim=-1)
+        )
+
+        self.critic = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, 1)
+        )
+
+    def action(self, state):
+        with torch.no_grad():
+            action_probs = self.actor(state).detach()
+            dist = Categorical(action_probs)
+            action = dist.sample()
+            action_log_prob = dist.log_prob(action).detach()
+            state_value = self.critic(state).detach()
+
+        return action, action_log_prob, state_value
+    
+    def evaluate(self, state, action):
+
+        # the evaluate function is not detached 
+
+        action_probs = self.actor(state)
+        dist = Categorical(action_probs)
+        action_log_prob = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+        state_value = self.critic(state)
+
+        return action_log_prob, state_value, dist_entropy
+    
+class PPO:
+    def __init__(self, state_dim, action_dim, actor_lr, critic_lr, gamma, 
+                 K_epochs, eps_clip):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.gamma = gamma
+        self.eps_clip = eps_clip
+        self.K_epochs = K_epochs
+
+        self.buffer = RolloutBuffer()
+
+        self.policy = ActorCritic(state_dim, action_dim)
+
+        # update actor and critic jointly
+        self.optimizer = torch.optim.Adam([
+            {'params': self.policy.actor.parameters(), 'lr': actor_lr},
+            {'params': self.policy.critic.parameters(), 'lr': critic_lr}
+        ])
+
+        self.old_policy = ActorCritic(state_dim, action_dim)
+        self.old_policy.load_state_dict(self.policy.state_dict())
+
+        self.MSELoss = nn.MSELoss()
+
+    def select_action(self, state):
+        with torch.no_grad():
+            state = torch.FloatTensor(state)
+            # action, action_log_probs, state_value = self.old_policy.action(state)
+            action, action_log_probs, state_value = self.policy.action(state)
+
+        self.buffer.states.append(state)
+        self.buffer.actions.append(action)
+        self.buffer.log_probs.append(action_log_probs)
+        self.buffer.state_values.append(state_value)
+
+        return action.item()
+
+    def update(self):
+        rewards = []
+        discounted_reward = 0
+        for reward, is_terminal in zip(reversed(self.buffer.rewards), reversed(self.buffer.is_terminals)):
+            if is_terminal:
+                discounted_reward = 0
+            discounted_reward = reward + (self.gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+        
+        rewards = torch.tensor(rewards, dtype=torch.float32)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-7)
+
+        # the buffer store the detached states, actions, log_probs, state_values
+        old_states = torch.squeeze(torch.stack(self.buffer.states, dim=0))
+        old_actions = torch.squeeze(torch.stack(self.buffer.actions, dim=0))
+        old_logprobs = torch.squeeze(torch.stack(self.buffer.log_probs, dim=0))
+        old_state_values = torch.squeeze(torch.stack(self.buffer.state_values, dim=0))
+
+        advantage = rewards - old_state_values
+
+        for _ in range(self.K_epochs):
+
+            # not detached
+            log_probs, state_values, dis_entropy = self.policy.evaluate(old_states, old_actions)
+            state_values = torch.squeeze(state_values)  
+            
+            ratios = torch.exp(log_probs - old_logprobs)
+            surr1 = ratios * advantage
+            surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantage
+            
+            loss = -torch.min(surr1, surr2) + 0.5 * self.MSELoss(state_values, rewards) - 0.01 * dis_entropy
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
+        
+        self.old_policy.load_state_dict(self.policy.state_dict())
+        self.buffer.clear()
+
+    def save(self, path):
+        torch.save(self.policy.state_dict(), path)
+
+    def load(self, path):
+        self.old_policy.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
+        self.policy.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
---
+++ main.py
@@ -0,0 +1,106 @@
+import os
+import time
+from datetime import datetime
+import torch
+import numpy as np
+import gymnasium as gym
+from tqdm import tqdm
+
+from PPO import PPO
+
+def eval_model(epoch):
+    env_name = 'CartPole-v1'
+    env = gym.make(env_name)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+
+    ppo_agent = PPO(state_dim, action_dim, 0, 0, 0, 0, 0)
+
+    ppo_agent.load(f"./models/ppo_{epoch}.pkl")
+
+    test_episodes = 1
+    rewards = []
+
+    for _ in range(test_episodes):
+        state = env.reset()[0]
+        current_ep_reward = 0
+        done = False
+
+        while not done:
+            action = ppo_agent.select_action(state)
+            next_state, reward, done, truncated, info = env.step(action)
+            done = done or truncated
+
+            state = next_state
+
+            current_ep_reward += reward
+
+        rewards.append(current_ep_reward)
+
+    return np.mean(rewards)
+
+def train():
+    env_name = 'CartPole-v1'
+
+    max_ep_len = 500
+    max_training_timesteps = 5e5
+
+    eval_freq = max_ep_len * 4
+
+    K_epochs = 40
+    eps_clip = 0.2
+    gamma = 0.99
+    actor_lr = 0.0003
+    critic_lr = 0.001
+    EPOCH = 10000
+
+    random_seed = 0
+
+    env = gym.make(env_name)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+    # env.seed(random_seed)
+
+    ppo_agent = PPO(state_dim, action_dim, actor_lr, critic_lr, gamma, K_epochs, eps_clip)
+
+    time_stamp = 0
+    epoch = 0
+
+    while time_stamp <= max_training_timesteps:
+
+        state = env.reset()[0]
+        current_ep_reward = 0
+        done = False
+
+        epoch += 1
+        
+        for t in range(1, max_ep_len + 1):
+
+            time_stamp += 1
+
+            action = ppo_agent.select_action(state)
+            next_state, reward, done, truncated, info = env.step(action)
+            done = done or truncated
+
+            state = next_state
+
+            ppo_agent.buffer.rewards.append(reward)
+            ppo_agent.buffer.is_terminals.append(done)
+
+            current_ep_reward += reward
+
+            if time_stamp % eval_freq == 0:
+
+                ppo_agent.update()
+                ppo_agent.save(f"./models/ppo_{epoch}.pkl")
+                reward_mean = eval_model(epoch)
+                print(f'Epoch {epoch}, train reward: {current_ep_reward}, test reward: {reward_mean}')
+
+            if done:
+                break
+
+if __name__ == '__main__':
+    train()
